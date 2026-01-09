54ffe5a358169b833da28a82a2ebbc41800c350c
nikitaugale8080-design <nikitaugale8080@gmail.com>
2026-01-09 12:36:03 +0530

    Update MySQL connection details for production

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"a2z45_shoes_ecom"
+    host:"btnjt8n91ymerailjgpq-mysql.services.clever-cloud.com",
+    user:"ufqs04qcw2u13tu3",
+    password:"0fRCsN5w6OpuLmHXnqPB",
+    database:"btnjt8n91ymerailjgpq"
 });
 var exe = util.promisify(connection.query).bind(connection);
 
